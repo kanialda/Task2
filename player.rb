b19f47412e7c6f4a6f4f6d29f15e6d17e8456d92
Lisda Kania Yuliani <kanialisda@gmail.com>
2016-07-26 08:21:00 +0700

    Modify player.rb

--- player.rb
+++ player.rb
@@ -7,34 +7,81 @@ class Player
         @manna = 40
     end
     
-    def self.attack
+    def self.attack 
        @manna -= 10
     end
     
-    def self.defend
+    def self.defend 
       @blood -= 25
     end
 end
 
 class Main
   @players = Hash.new
-  def self.menu
-  puts " # ============================== #
+  
+  def self.menu_new
+    puts " # ============================== #
          # Welcome to the Battle Arena #
          # ------------------------------------------------- ---- #
          # Description: #
          # 1. type new to create a character #
          # 2. type start to begin the fight #"
-         input_p = gets.chomp
+    input_p = gets.chomp
+         if input_p.eql? "new"
+         Main.add_players
+         elsif input_p.eql? "start"
+         puts "Nothing players. Please add new player first"
+         Main.menu_start
+         else
+         puts "didn't know input"
+         end
+  end
+  
+  def self.menu_start
+    puts " # ============================== #
+         # Welcome to the Battle Arena #
+         # ------------------------------------------------- ---- #
+         # Description: #
+         # 1. type new to create a character #
+         # 2. type start to begin the fight #"
+    input_p = gets.chomp
          if input_p.eql? "new"
          Main.add_players
          elsif input_p.eql? "start"
          Main.start_play
          else
          puts "didn't know input"
-         Main.menu
          end
-  end 
+  end
+  
+  def self.menu_max
+    puts " # ============================== #"
+    puts " Welcome to the Battle Arena #"
+    puts "# ------------------------------------------------- ---- #"
+    puts "# Description: #"
+    puts "# 1. type new to create a character #"
+    puts "# 2. type start to begin the fight #"
+    input_p = gets.chomp
+         if input_p.eql? "new"
+         puts "Max player reached"
+         puts "Let's start play!"
+         elsif input_p.eql? "start"
+         Main.start_play
+         else
+         puts "didn't know input"
+         end
+     puts " # ============================== #
+         # Welcome to the Battle Arena #
+         # ------------------------------------------------- ---- #
+         # Please type start to begin the fight #"
+     input_p = gets.chomp
+         if input_p.eql? "start"
+         Main.start_play
+         else
+         puts "didn't know input"
+         end
+  end
+  
   def self.add_players
     
     puts "# ============================== #
@@ -45,26 +92,21 @@ class Main
          # 2. type start to begin the fight #
          # ------------------------------------------------- ---- #
          # * Max player 2 or 3 #
-         # ------------------------------------------------- ---- #
-         # Put Player Name:"
-         name_p = gets.chomp  
-         @players[name_p]= Player.new(name_p)
-         if @players.count < 4
-         puts "# ============================== #
-         # Welcome to the Battle Arena #
-         # ------------------------------------------------- ---- #
-         # Description: #
-         # 1. type new to create a character #
-         # 2. type start to begin the fight #
-         # ------------------------------------------------- ---- #
-         # Current Player: 1 #"
-         Main.loop_player
-         puts "# * Max player 2 or 3 #
          # ------------------------------------------------- ---- #"
-         else
-           puts "Max player reached"
+         n=0
+         while n < 3 do
+         puts "# Put Player Name:"
+         @name_p = gets.chomp  
+         @players[@name_p]= Player.new(@name_p)
+         n=n+1
          end
-         Main.menu   
+         puts "# Current Player #"
+         Main.loop_player
+         puts "# * Max player 2 or 3 #"
+         puts "# ------------------------------------------------- #"
+         puts "Max player reached"
+         Main.menu_max
+         
   end 
   
   def self.loop_player
@@ -77,25 +119,25 @@ class Main
     puts "# ============================== #
           # Welcome to the Battle Arena #
           # ------------------------------------------------- ---- #"
-          @players.each do |key, p|
-          puts "Battle Start:
-          who will attack: #{@players[key].name}"
-          puts "who attacked
-          : #{@players[key].name}"
-          
-          puts "# ============================== #
-          # Welcome to the Battle Arena #
-          # ------------------------------------------------- ---- #
-          Battle Start:"
-          # Player.attack
-          # Player.defend
-          puts "who will attack: #{@players[key].name}"
-          puts "who attacked: #{@players[key].name}"
-          puts "Description:
-          #{@players[key].name} manna = #{@players[key].manna}, blood = #{@players[key].blood}"
-          
-          end
+            puts "Battle Start:
+            who will attack: "
+            player_1 = gets.chomp  
+            @players[player_1]= Player.new(player_1)
+            puts "who attacked: "
+            player_2 = gets.chomp
+            @players[player_2]= Player.new(player_2)
+            puts "# ============================== #
+            # Welcome to the Battle Arena #
+            # ------------------------------------------------- ---- #
+            Battle Start:"
+            puts "who will attack: #{@players[player_1].name}"
+            puts "who attacked: #{@players[player_2].name}"
+            
+            puts "Description:"
+            @players.each do |key, p|
+            puts "name: #{@players[key].name}, blood: #{@players[key].blood}, manna: #{@players[key].manna}"
+            end
   end
   
 end
-Main.menu
+Main.menu_new
